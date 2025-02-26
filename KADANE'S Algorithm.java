82bf9f3c3fdb1aa19f2ae3b9322b99323640860c
Nandkishor Dhadhal <150511770+Nand68@users.noreply.github.com>
2025-02-26 15:21:24 +0530

    Create KADANE'S Algorithm.java

---
+++ KADANE'S Algorithm.java
@@ -0,0 +1,17 @@
+public class KadanesAlgorithm {
+    public static int maxSubarraySum(int[] arr) {
+        int maxSum = Integer.MIN_VALUE;
+        int currentSum = 0;
+
+        for (int num : arr) {
+            currentSum += num;
+            maxSum = Math.max(maxSum, currentSum);
+
+            if (currentSum < 0) {
+                currentSum = 0;
+            }
+        }
+
+        return maxSum;
+    }
+}
